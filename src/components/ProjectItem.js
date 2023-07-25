b9296211dc5a2fe7a51fd582f2946d1244ca2d9f
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-07-25 13:47:06 +0700

    perbaikan tampilan pada project item

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ProjectItem({ logo, altimg, projectName, reward, target, link }) {
     return (
-        <div className="flex bg-primary-purple bg-opacity-60 p-4 mb-12 rounded-xl justify-between items-center">
+        <div className="grid grid-cols-5 bg-primary-purple bg-opacity-60 p-4 mb-12 rounded-xl justify-between items-center">
             <img src={logo} alt={altimg} className="w-14" />
             <div className="text-white">
                 <p className="font-semibold">{projectName}</p>
@@ -17,8 +17,8 @@ function ProjectItem({ logo, altimg, projectName, reward, target, link }) {
                 <p>Target</p>
             </div>
             <a href={link}>
-                <div className="bg-white bg-opacity-60 px-16 py-3 rounded-xl">
-                    <p className="font-bold text-primary-purple">View Bounty</p>
+                <div className="bg-white bg-opacity-60 py-3 rounded-xl">
+                    <p className="font-bold text-primary-purple text-center">View Bounty</p>
                 </div>
             </a>
         </div>
